4e51421ef8babdb98227480f34787f4dc4664062
github username <github email address>
2021-04-19 20:59:26 -0400

    commit tree 6451d4f13a2addbc327b099daa802688d5300bf7

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,25 @@
 require 'pry'
 class Application
-    @@item = []
+    @@items = [Item.new("banana", 3), Item.new("apple", 1), Item.new("dyson", 700)]
 
     def call(env)
         resp = Rack::Response.new
         req = Rack::Request.new(env)
+#your application should only accept the items --
+#hard code in item array to give it data
 
         if req.path.match (/items/)
-          items_price = req.path.split("/items/").last.price
-          resp.write items_price
+            
+          item_name = req.path.split("/items/").last
+           if found =  @@items.find { |item| item.name == item_name}
+                # binding.pry
+                 resp.write found.price
+           else
+            resp.write "Item not found"
+            resp.status = 400
+            end
+
+          
         else
           resp.write "Route not found"
           resp.status = 404
